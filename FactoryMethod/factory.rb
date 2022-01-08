6d2cdc08099856f57674dc21c60e371d1925b322
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-09 00:21:11 +0530

    Updated

--- FactoryMethod/factory.rb
+++ FactoryMethod/factory.rb
@@ -1,13 +1,14 @@
 Dir[File.dirname(__FILE__) + "/lib/animal_types/*.rb"].each {|file| require file }
-
-def get_animal(type)
-    if type == "lion"
-        return Lion.new
-    elsif type == "goat"
-        return Goat.new 
-    elsif type == "dog"
-        return Dog.new 
-    else
-        raise "#{type} is not valid type of animal"
+class Factory
+    def self.call(type)
+        if type == "lion"
+            return Lion.new
+        elsif type == "goat"
+            return Goat.new 
+        elsif type == "dog"
+            return Dog.new 
+        else
+            raise "#{type} is not valid type of animal"
+        end
     end
 end
--- FactoryMethod/main.rb
+++ FactoryMethod/main.rb
@@ -1,5 +1,5 @@
 require_relative 'factory'
 
-animal = get_animal(ARGV[0])
+animal = Factory.call(ARGV[0])
 animal.speak
 animal.food
